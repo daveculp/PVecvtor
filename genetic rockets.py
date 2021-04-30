55dcb3af08c8c3e32d9e01d9c4de258e367d1508
daveculp <dwculp@gmail.com>
2021-04-30 15:57:48 -0500

    Add files via upload

--- genetic rockets.py
+++ genetic rockets.py
@@ -30,39 +30,53 @@ class DNA():
 		self.genes = []
 
 		gene = PVector.random2D()
-		gene.mult(5)
+		gene.mult(.1)
 		self.genes.append(gene)
 		for j in range(1, num_genes):
-			if random.randint(1,100) < 90:
+			if random.randint(1,100) < 95:
 				self.genes.append(self.genes[j-1])
 			else:
 				gene = PVector.random2D()
-				gene.mult(5)
+				gene.mult(.1)
 				self.genes.append(gene)
 				
 class Rocket():
-	def __init__(self, x,y, num_genes):
-		self.speed = PVector(0,0)
+	def __init__(self, x,y, num_genes, target, obstacle):
+		self.velocity = PVector(0,0)
 		self.location = PVector(x,y)
-		self.accel = PVector.random2D
+		self.acceleration = PVector(0,0)
 		self.radius = 2
+		self.gene_counter = 0
 		self.color = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
 		self.stopped = False
 		self.hit_target = False
 		self.rect = 0
 		self.dna = DNA(num_genes)
 		self.fitness = 0
+		self.target = target
+		self.obstacle = obstacle
 
 	def find_fitness(self, target):
 		distance = math.hypot(self.location.x - target.x, self.location.y-target.y)
 		self.fitness = pow((1/distance),2)
+		if self.hit_target == True:
+			self.fitness *= 2
 		if self.stopped:
 			self.fitness = self.fitness*.1
 
-		
 	def update(self, position):
 		if not self.stopped:
-			self.location.add(self.dna.genes[position])
+			self.apply_force(self.dna.genes[position])
+			self.velocity.add(self.acceleration)
+			self.location.add(self.velocity)
+			self.acceleration.mult(0)
+			self.check_bounds()
+			self.check_target()
+			self.detect_collision()
+			#self.location.add(self.dna.genes[position])
+	
+	def apply_force(self,f):
+		self.acceleration.add(f)
 		
 	def draw(self):
 		if not self.stopped:
@@ -70,11 +84,18 @@ class Rocket():
 			y = self.location.y
 			self.rect = pygame.draw.circle( screen, self.color, ( int(x),int(y) ), self.radius)
 	
-	def detect_collision(self, obstacle):
+	def detect_collision(self):
 		if not self.stopped:
-			if self.rect.colliderect(obstacle):
+			if self.rect.colliderect(self.obstacle):
 				self.stopped = True
 				
+	def check_target(self):
+		x = self.location.x
+		y = self.location.y
+		distance = math.hypot(self.location.x - self.target.x, self.location.y-self.target.y)
+		if distance < 20:
+			self.hit_target = True
+		
 	def check_bounds(self):
 		if self.location.x < 0 or self.location.x > 799:
 			self.stopped = True
@@ -82,17 +103,17 @@ class Rocket():
 			self.stopped = True
 			
 class Population():
-	def __init__(self, mutation_rate, num_individuals, num_genes):
+	def __init__(self, mutation_rate, num_individuals, num_genes, target, obstacle):
 		self.mutation_rate = mutation_rate
 		self.rocket_population = []
 		self.generations = 0
-		self.populate(num_individuals, num_genes)
+		self.populate(num_individuals, num_genes, target, obstacle)
 	
-	def populate(self, num_individuals, num_genes):
+	def populate(self, num_individuals, num_genes, target, obstacle):
 		for i in range(num_individuals):
 			x = SCREEN_WIDTH//2
 			y = SCREEN_HEIGHT-1
-			rocket = Rocket(x,y, num_genes)
+			rocket = Rocket(x,y, num_genes, target, obstacle)
 
 			self.rocket_population.append(rocket)
 		
@@ -104,12 +125,12 @@ screen = pygame.display.set_mode( [SCREEN_WIDTH, SCREEN_HEIGHT] )
 fps_clock = pygame.time.Clock()
 obstacle = pygame.Rect(250,350,200,100)
 target = PVector(400,20)
-num_genes = 300
+num_genes = 200
 num_rockets = 200
 
 
 def main(args):
-	rockets = Population(.1, num_rockets, num_genes)
+	rockets = Population(.1, num_rockets, num_genes, target, obstacle)
 	running = True
 
 	while running:
@@ -126,8 +147,6 @@ def main(args):
 			
 			for rocket in rockets.rocket_population:			
 				rocket.draw()
-				rocket.detect_collision(obstacle)
-				rocket.check_bounds()
 				rocket.update(j)
 
 			pygame.display.flip()
